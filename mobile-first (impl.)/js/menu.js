55221af578fea5a212e9f761462c1e7ed74c61ba
Juan Luis Filgueiras Rilo <juan.filgueiras.rilo@udc.es>
2018-01-06 00:07:03 +0100

    Añadido tab flexible

--- mobile-first (impl.)/js/menu.js
+++ mobile-first (impl.)/js/menu.js
@@ -1,22 +1,25 @@
 function toggleSidenav() {
 	if (document.body.classList.contains('sidenav-active')) {
-		var dropdowns = document.getElementsByClassName('sub-menu');
-		var i;
-		for (i = 0; i < dropdowns.length; i++) {
-			var openDropdown = dropdowns[i];
-			if (openDropdown.classList.contains('dropdown')) {
-				openDropdown.classList.remove('dropdown');
-			}
-		}
+		closeDrops();
 	}
 	document.body.classList.toggle('sidenav-active');
 }
 
 function dropdown(id) {
-	
+	closeDrops();
 	document.getElementById(id).classList.toggle('dropdown');	
 }
 
+function closeDrops() {
+	var dropdowns = document.getElementsByClassName('sub-menu');
+	var i;
+	for (i = 0; i < dropdowns.length; i++) {
+		var openDropdown = dropdowns[i];
+		if (openDropdown.classList.contains('dropdown')) {
+			openDropdown.classList.remove('dropdown');
+		}
+	}	
+}
 /*function color(id) {
 	var is_mobile = false;
 
